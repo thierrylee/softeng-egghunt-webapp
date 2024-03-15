7c06fe25f5065257b7ce7abdfbc8e6e0d16b7de3
Thierry LEE <thierry.leet@gmail.com>
2024-03-15 18:16:05 +0100

    Add message on successful scan

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "a6d290f83a1d707beb4f5575c8f520a0",
-"index.html": "c39a19cca96392476abbec493e66f254",
-"/": "c39a19cca96392476abbec493e66f254",
-"main.dart.js": "4ac04be21995587b56b9814d4a0b6b9c",
+"index.html": "aa140b84418ebb7991698c6e260fef95",
+"/": "aa140b84418ebb7991698c6e260fef95",
+"main.dart.js": "6ce2dbbcd31a68f311bbfa7ab34820af",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "7a22d09c96ccc285719f422799c21306",
 "icons/Icon-192.png": "7331ff85c4169f093d32d48e270c6e43",
